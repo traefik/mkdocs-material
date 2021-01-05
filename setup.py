ed9a3b9ffe0d2cefe60640fd872b42a89cd567d4
Matthieu Hostache <matthieu.hostache@gmail.com>
2021-01-05 09:57:14 +0100

    Add license + use right author/package name

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
+# Copyright (c) 2016-2020 Martin Donath <martin.donath@squidfunk.com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+
 import json
 from setuptools import setup, find_packages
 
@@ -18,19 +39,19 @@ with open("README.md", encoding = "utf-8") as data:
 
 # Package description
 setup(
-    name = "mkdocs-traefiklabs-test",
+    name = "mkdocs-traefiklabs",
     version = package["version"],
     url = "http://traefik.io",
     license = package["license"],
     description = package["description"],
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    author = "matthieuh",
+    author = "traefiklabs",
     keywords = package["keywords"],
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
-        "License :: OSI Approved :: Apache Software License",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: JavaScript",
         "Programming Language :: Python",
         "Topic :: Documentation",
@@ -42,7 +63,7 @@ setup(
     install_requires = install_requires,
     entry_points = {
         "mkdocs.themes": [
-            "traefik-labs-test = material",
+            "traefik-labs = material",
         ]
     },
     zip_safe = False
