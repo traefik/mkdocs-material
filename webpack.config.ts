a8b48d8d8c9d5f2eff45719586a332e3a9b67157
Arpit Bansal <arpit@codalent.com>
2026-03-03 20:14:31 +0530

    Improve docs website operations

--- webpack.config.ts
+++ webpack.config.ts
@@ -202,7 +202,8 @@ export default (_env: never, args: Configuration): Configuration[] => {
         "assets/javascripts/bundle":    "src/assets/javascripts",
         "assets/stylesheets/main":      "src/assets/stylesheets/main.scss",
         "assets/stylesheets/overrides": "src/assets/stylesheets/overrides.scss",
-        "assets/stylesheets/palette":   "src/assets/stylesheets/palette.scss"
+        "assets/stylesheets/palette":   "src/assets/stylesheets/palette.scss",
+        "assets/stylesheets/cookie-banner": "src/assets/stylesheets/cookie-banner.scss"
       },
       output: {
         path: path.resolve(__dirname, "material"),
